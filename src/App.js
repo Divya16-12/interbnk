b0461774ea031802e5f2b6b3049b820185e590f8
Divya16-12 <129410248+Divya16-12@users.noreply.github.com>
2023-05-16 10:13:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div classname="App">
+    <h1><center> Internet Banking</center></h1>
+    <h1><center> SMD BANK CO.LTD</center></h1>
+
+    <div id="navigation">
+    <ul>
+	<li><a href="create1.html">NEW CUSTOMER</a></li>
+    <li><a href="login.html">LOGIN</a></li>
+    
+    <li><a href="about.jsp">ABOUT US</a></li>
+    </ul>
+</div>
+    
+    <p> <b>Internet banking allows the user to conduct financial transactions via the internet</b>
+    <div id="heade1"><h1>Results</h1></div>
+   <b> This is  Global Business bank which is under business and finance category for your website design. Freedesignertemplates.com is a free collection of site templates with HTML layouts which are frequently updated
+         
+         These web design layouts are developed by the professional designers for you to make your task easy in creating a website and are easy to download. 
+   </b>
+    </p>
+
+    <div id="heade2"><h1>Opportunities</h1></div>
+        <p> <b>These Global Business bank would be idle for building a successful online businesses.
+          The entire website   are pre-designed websites all you need to do is adding your own personal content. </b>
+          </p>
+
+          <div id="heade3"><h1>Solutions</h1></div>
+        <p><b>There are various other template categories like Art, Finance, Nature, Night club, Communication, Computer, Music, Religious, Fashion, Holiday, Travel, Health, Medicine. This free Global Business bank which is built using XHTML and CSS</b>.
+          </p>
+        
+
+          
+
+
+          <div id="footer_copyright" >
+ 
+ 
+<p>Internet banking is also known as Net banking, online banking, or digital banking. Internet banking was introduced to make the banking system more convenient and safer.</p>
+
+Copyright © Your Company Name</div>
+</div>
+
+    
+    
+    
+  
+
+      
+     
+   );
+}
+
+export default App;
